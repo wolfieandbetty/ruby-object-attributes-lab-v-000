303102eea0620036bec24358cfd8fec48a0f1d27
Cat Norris <cannenorris@yahoo.com>
2018-02-02 06:44:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,10 +10,12 @@ class Dog
     @name
   end
 
+# This method assigns instance variable @breed, the value of the argument passed into the method
 def breed=(dogs_breed)
   @breed=dogs_breed
 end
 
+# This method reads and returns the value of instance variable @breed that was set in #breed=
 def breed
   @breed
 end
